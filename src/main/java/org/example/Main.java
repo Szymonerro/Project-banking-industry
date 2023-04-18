c26b4b8dee9d03e091d4ac6d973f4ba5cf7aa677
Szymon Rosiak <artox_27@o2.pl>
2023-04-18 15:25:30 +0200

    Hierarchy of Bank classes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Objects of implemented classes
+
+        Bank bank = new Bank();
+        BankAccount bankAccount = new BankAccount();
+        Customer customer = new Customer();
+        BankEmployee bankEmployee = new BankEmployee();
+        BankManager bankManager = new BankManager();
+        Transaction transaction = new Transaction();
+        TransactionHistory transactionHistory = new TransactionHistory();
+        AccountType accountType = new AccountType();
+        SavingsAccount savingsAccount = new SavingAccount();
+        InvestmentAccount investmentAccount = new InvestmentAccount();
+        BusinessAccount businessAccount = new BusinessAccount();
+        CardType cardType = new CardType();
+        CreditCard creditCard = new CreditCard();
+        DebitCard debitCard = new DebitCard();
+        Loan loan = new Loan();
+
+    }
+}
+
